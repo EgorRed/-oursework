61648859be1e5febc29a8e2f1140a1f219550ba7
EgorRed <egor.red02@gmail.com>
2023-12-14 19:35:58 +0300

    try to integrate products tables1

---
+++ AccountingForExpirationDates/Data/ProductDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AccountingForExpirationDates.Model.Product;
+using AccountingForExpirationDates.UserData;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountingForExpirationDates.Data
+{
+    public class ProductDbContext : DbContext
+    {
+
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<CategoryModel> Category { get; set; }
+        public DbSet<PhotoModel> Photo { get; set; }
+
+        public ProductDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
